eb447950a516a994f7c6160f37bf210f38f21464
Sarah Davidsen <sarahdavidsen@desktop-kof78ba.kea.local>
2019-09-03 12:34:11 +0200

    startout

---
+++ art.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DomContentloaded", start);
+
+function start() {
+  let box = document.querySelector("div");
+  for (let i = 100; i <= 300; i += 20) {
+    console.log(i);
+  }
+}
